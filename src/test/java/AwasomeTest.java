fa8047186a86bd633d6442e162d71149153596a0
pinar3146 <pinaryanar3146@hotmail.com>
2020-10-06 22:18:43 +0200

    added more stuff practice 2

--- src/test/java/AwasomeTest.java
+++ src/test/java/AwasomeTest.java
@@ -6,5 +6,6 @@ public class AwasomeTest {
     //I just pulled the new change from github that I did not have locally
     //and now my project is in syc with remote repository.
 
-    //Now I made more awasome changes  and I am about to push thisd
+    //Now I made more awasome changes  and I am about to push this
+    //adding more line
 }
