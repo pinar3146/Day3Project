f7b7c900a57be54378793821f34ab644f4b44cc0
pinar3146 <pinaryanar3146@hotmail.com>
2020-10-06 21:59:19 +0200

    added more awesome stuff

--- src/test/java/AwasomeTest.java
+++ src/test/java/AwasomeTest.java
@@ -3,5 +3,8 @@ public class AwasomeTest {
     //green means is staging area
     //grey in .idea folder means ignored
     //when the color is normal it means it is committed no changes
+    //I just pulled the new change from github that I did not have locally
+    //and now my project is in syc with remote repository.
 
+    //Now I made more awasome changes  and I am about to push thisd
 }
