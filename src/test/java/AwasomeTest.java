4531698b847c71b77af7e8284d13e74cdf56cfe6
pinar3146 <pinaryanar3146@hotmail.com>
2020-10-06 21:46:15 +0200

    added new file for awesome feature

---
+++ src/test/java/AwasomeTest.java
@@ -0,0 +1,7 @@
+public class AwasomeTest {
+    //red means is untracked
+    //green means is staging area
+    //grey in .idea folder means ignored
+    //when the color is normal it means it is committed no changes
+
+}
